e8ce4488f8e21d399953b2b1c05b66d82010b218
smithy_dll <none@none>
2009-02-13 08:40:37 +0000

    Missing Files

---
+++ BoxSocial.Internals/Contact.cs
@@ -0,0 +1,163 @@
+﻿/*
+ * Box Social™
+ * http://boxsocial.net/
+ * Copyright © 2007, David Lachlan Smith
+ * 
+ * $Id:$
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Xml;
+using System.Xml.Serialization;
+using BoxSocial.IO;
+using BoxSocial.Internals;
+
+namespace BoxSocial.Internals
+{
+	[DataTable("contacts")]
+	public class Contact : NumberedItem
+	{
+		[DataField("contact_id", DataFieldKeys.Primary)]
+		private long contactId;
+		[DataField("contact_name_display", 64)]
+        private string contactDisplayName;
+		[DataField("contact_name_title", 8)]
+        private string nameTitle;
+        [DataField("contact_name_first", 36)]
+        private string nameFirst;
+        [DataField("contact_name_middle", 36)]
+        private string nameMiddle;
+        [DataField("contact_name_last", 36)]
+        private string nameLast;
+        [DataField("contact_name_suffix", 8)]
+        private string nameSuffix;
+		
+		public long ContactId
+		{
+			get
+			{
+				return contactId;
+			}
+		}
+		
+		/// <summary>
+        /// Gets the contact's Display name
+        /// </summary>
+        public string DisplayName
+        {
+            get
+            {
+                if (contactDisplayName == "")
+                {
+                    return FirstName + " " + LastName;
+                }
+                else
+                {
+                    return contactDisplayName;
+                }
+            }
+            set
+            {
+                SetProperty("contactDisplayName", value);
+            }
+        }
+		
+		public string Title
+        {
+            get
+            {
+                return nameTitle;
+            }
+            set
+            {
+                SetProperty("nameTitle", value);
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return nameFirst;
+            }
+            set
+            {
+                SetProperty("nameFirst", value);
+            }
+        }
+
+        public string MiddleName
+        {
+            get
+            {
+                return nameMiddle;
+            }
+            set
+            {
+                SetProperty("nameMiddle", value);
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return nameLast;
+            }
+            set
+            {
+                SetProperty("nameLast", value);
+            }
+        }
+
+        public string Suffix
+        {
+            get
+            {
+                return nameSuffix;
+            }
+            set
+            {
+                SetProperty("nameSuffix", value);
+            }
+        }
+		
+		public Contact(Core core)
+			: base(core)
+		{
+		}
+		
+		public override long Id
+		{
+			get 
+			{
+				return contactId;
+			}
+		}
+
+		public override string Uri
+		{
+			get 
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+}

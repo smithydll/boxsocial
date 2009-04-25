c64d60d253239870010fe1dbd4aebb1b811ba4e6
smithy_dll <none@none>
2009-04-25 01:53:48 +0000

    missing file

---
+++ BoxSocial.Internals/PerformanceMeter.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Box Social™
+ * http://boxsocial.net/
+ * Copyright © 2007, David Lachlan Smith
+ * 
+ * $Id:$
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxSocial.Internals
+{
+    public class PerformanceMeter
+    {
+        public long last;
+        private List<string> events;
+
+        public PerformanceMeter()
+        {
+            last = DateTime.Now.Ticks;
+            events = new List<string>();
+            Add("Initialised Counter");
+        }
+
+        public void Add(string eventName)
+        {
+            events.Add(string.Format("{1}\t{2}",
+                DateTime.Now.Ticks / 10000000.0, (DateTime.Now.Ticks - last) / 10000000.0, eventName));
+            last = DateTime.Now.Ticks;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string s in events)
+            {
+                sb.AppendLine(s + "<br />");
+            }
+
+            return sb.ToString();
+        }
+    }
+}

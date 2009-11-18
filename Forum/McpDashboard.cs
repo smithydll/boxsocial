c404661d60a8cd53b46d659119e172dc373f91e1
smithy_dll <none@none>
2009-11-18 10:56:04 +0000

    Work on moderator control panel (MCP)

---
+++ Forum/McpDashboard.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Box Social™
+ * http://boxsocial.net/
+ * Copyright © 2007, David Lachlan Smith
+ * 
+ * $Id:$
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using BoxSocial;
+using BoxSocial.Internals;
+using BoxSocial.IO;
+
+namespace BoxSocial.Applications.Forum
+{
+    [AccountModule("dashboard")]
+    public class McpDashboard : ModeratorControlPanelModule
+    {
+
+        public McpDashboard(Account account)
+            : base(account)
+        {
+            // TODO: subscription management
+        }
+
+        protected override void RegisterModule(Core core, EventArgs e)
+        {
+        }
+
+
+        public override string Name
+        {
+            get
+            {
+                return "Dashboard";
+            }
+        }
+
+        public override int Order
+        {
+            get
+            {
+                return 1;
+            }
+        }
+    }
+}
---
+++ Forum/McpMain.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * Box Social™
+ * http://boxsocial.net/
+ * Copyright © 2007, David Lachlan Smith
+ * 
+ * $Id:$
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Web;
+using BoxSocial.Internals;
+using BoxSocial.IO;
+
+namespace BoxSocial.Applications.Forum
+{
+    [AccountSubModule(AppPrimitives.Group, "dashboard", "main", true)]
+    public class McpMain : ModeratorControlPanelSubModule
+    {
+        public override string Title
+        {
+            get
+            {
+                return "Main";
+            }
+        }
+
+        public override int Order
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        public McpMain()
+        {
+            this.Load += new EventHandler(AccountOverview_Load);
+            this.Show += new EventHandler(AccountOverview_Show);
+        }
+
+        void AccountOverview_Load(object sender, EventArgs e)
+        {
+        }
+
+        void AccountOverview_Show(object sender, EventArgs e)
+        {
+            template.SetTemplate("account_landing.html");
+
+        }
+    }
+}

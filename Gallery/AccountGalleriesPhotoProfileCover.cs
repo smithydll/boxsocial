3fd105e2a344fddd7b552f7a6713761a856f3cd9
smithy_dll <none@none>
2013-03-20 22:51:27 +1100

    Cover Photo

---
+++ Gallery/AccountGalleriesPhotoProfileCover.cs
@@ -0,0 +1,118 @@
+﻿/*
+ * Box Social™
+ * http://boxsocial.net/
+  * Copyright © 2007, David Smith
+ * 
+ * $Id:$
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Web;
+using BoxSocial.Internals;
+using BoxSocial.IO;
+
+namespace BoxSocial.Applications.Gallery
+{
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    [AccountSubModule("galleries", "profile-cover")]
+    public class AccountGalleriesPhotoProfileCover : AccountSubModule
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override string Title
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override int Order
+        {
+            get
+            {
+                return -1;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the AccountGalleriesPhotoDisplayPic class. 
+        /// </summary>
+        /// <param name="core">The Core token.</param>
+        public AccountGalleriesPhotoProfileCover(Core core)
+            : base(core)
+        {
+            this.Load += new EventHandler(AccountGalleriesPhotoProfileCover_Load);
+            this.Show += new EventHandler(AccountGalleriesPhotoProfileCover_Show);
+        }
+
+        void AccountGalleriesPhotoProfileCover_Load(object sender, EventArgs e)
+        {
+        }
+
+        void AccountGalleriesPhotoProfileCover_Show(object sender, EventArgs e)
+        {
+            AuthoriseRequestSid();
+
+            long pictureId = core.Functions.RequestLong("id", 0);
+
+            if (pictureId == 0)
+            {
+                core.Display.ShowMessage("Invalid submission", "You have made an invalid form submission. (0x07)");
+                return;
+            }
+
+            // check the image exists
+            // check the image is owned by the user trying to set it as their display picture
+            try
+            {
+                GalleryItem ugi = new GalleryItem(core, LoggedInMember, pictureId);
+
+                // check for public view permissions on the image
+                if (true)
+                {
+
+                    LoggedInMember.UserInfo.CoverPhotoId = pictureId;
+                    LoggedInMember.UserInfo.Update();
+
+                    core.Display.ShowMessage("Display Picture Changed", "You have successfully changed your cover photo.");
+                    return;
+                }
+                else
+                {
+                    core.Display.ShowMessage("Cannot set as display picture", "You must use a photo with public view permissions as your cover photo.");
+                    return;
+                }
+            }
+            catch (GalleryItemNotFoundException)
+            {
+                core.Display.ShowMessage("Cannot change cover photo", "You could not change your cover photo to the selected image.");
+                return;
+            }
+        }
+    }
+}
---
+++ Profile/AccountCoverPhoto.cs
@@ -0,0 +1,220 @@
+﻿/*
+ * Box Social™
+ * http://boxsocial.net/
+  * Copyright © 2007, David Smith
+ * 
+ * $Id:$
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using System.Web;
+using BoxSocial.Internals;
+using BoxSocial.IO;
+using BoxSocial.Applications.Gallery;
+
+namespace BoxSocial.Applications.Profile
+{
+    [AccountSubModule("profile", "cover-photo")]
+    public class AccountCoverPhoto : AccountSubModule
+    {
+        public override string Title
+        {
+            get
+            {
+                return "Cover Photo";
+            }
+        }
+
+        public override int Order
+        {
+            get
+            {
+                return 3;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the AccountDisplayPic class. 
+        /// </summary>
+        /// <param name="core">The Core token.</param>
+        public AccountCoverPhoto(Core core)
+            : base(core)
+        {
+            this.Load += new EventHandler(AccountCoverPhoto_Load);
+            this.Show += new EventHandler(AccountCoverPhoto_Show);
+        }
+
+        void AccountCoverPhoto_Load(object sender, EventArgs e)
+        {
+        }
+
+        void AccountCoverPhoto_Show(object sender, EventArgs e)
+        {
+            SetTemplate("account_cover_photo");
+
+            LoggedInMember.LoadProfileInfo();
+
+            if (!string.IsNullOrEmpty(LoggedInMember.UserThumbnail))
+            {
+                template.Parse("I_DISPLAY_PICTURE", LoggedInMember.UserThumbnail);
+            }
+
+            Save(new EventHandler(AccountDisplayPic_Save));
+        }
+
+        void AccountDisplayPic_Save(object sender, EventArgs e)
+        {
+            AuthoriseRequestSid();
+
+            string meSlug = "cover-photos";
+
+            BoxSocial.Applications.Gallery.Gallery profileGallery;
+            try
+            {
+                profileGallery = new BoxSocial.Applications.Gallery.Gallery(core, LoggedInMember, meSlug);
+            }
+            catch (InvalidGalleryException)
+            {
+                BoxSocial.Applications.Gallery.Gallery root = new BoxSocial.Applications.Gallery.Gallery(core, LoggedInMember);
+                profileGallery = BoxSocial.Applications.Gallery.Gallery.Create(core, LoggedInMember, root, "Cover Photos", ref meSlug, "All my uploaded cover photos");
+            }
+
+            if (profileGallery != null)
+            {
+                string title = "";
+                string description = "";
+                string slug = "";
+
+                try
+                {
+                    slug = core.Http.Files["photo-file"].FileName;
+                }
+                catch
+                {
+                    DisplayGenericError();
+                    return;
+                }
+
+                try
+                {
+                    MemoryStream stream = new MemoryStream();
+                    core.Http.Files["photo-file"].InputStream.CopyTo(stream);
+
+                    db.BeginTransaction();
+
+                    Image image = Image.FromStream(stream);
+                    int width = image.Width;
+                    int height = image.Height;
+
+                    RotateFlipType rotate = RotateFlipType.RotateNoneFlipNone;
+                    foreach (PropertyItem p in image.PropertyItems)
+                    {
+                        if (p.Id == 274)
+                        {
+                            switch ((int)p.Value[0])
+                            {
+                                case 1:
+                                    rotate = RotateFlipType.RotateNoneFlipNone;
+                                    break;
+                                case 2:
+                                    rotate = RotateFlipType.RotateNoneFlipX;
+                                    break;
+                                case 3:
+                                    rotate = RotateFlipType.Rotate180FlipNone;
+                                    break;
+                                case 4:
+                                    rotate = RotateFlipType.Rotate180FlipX;
+                                    break;
+                                case 5:
+                                    rotate = RotateFlipType.Rotate90FlipX;
+                                    break;
+                                case 6:
+                                    rotate = RotateFlipType.Rotate90FlipNone;
+                                    break;
+                                case 7:
+                                    rotate = RotateFlipType.Rotate270FlipX;
+                                    break;
+                                case 8:
+                                    rotate = RotateFlipType.Rotate270FlipNone;
+                                    break;
+                                default:
+                                    rotate = RotateFlipType.RotateNoneFlipNone;
+                                    break;
+                            }
+                        }
+                    }
+
+                    if (rotate != RotateFlipType.RotateNoneFlipNone)
+                    {
+                        image.RotateFlip(rotate);
+
+                        width = image.Width;
+                        height = image.Height;
+
+                        ImageFormat iF = ImageFormat.Jpeg;
+                        stream = new MemoryStream();
+                        image.Save(stream, iF);
+                        rotate = RotateFlipType.RotateNoneFlipNone;
+                    }
+
+                    string saveFileName = core.Storage.SaveFile(core.Storage.PathCombine(core.Settings.StorageBinUserFilesPrefix, "_storage"), stream);
+
+                    GalleryItem galleryItem = GalleryItem.Create(core, LoggedInMember, profileGallery, title, ref slug, core.Http.Files["photo-file"].FileName, saveFileName, core.Http.Files["photo-file"].ContentType, (ulong)core.Http.Files["photo-file"].ContentLength, description, 0, Classifications.Everyone, width, height);
+
+                    db.UpdateQuery(string.Format("UPDATE user_info SET user_icon = {0} WHERE user_id = {1}",
+                        galleryItem.Id, LoggedInMember.UserId));
+
+                    db.CommitTransaction();
+                    stream.Close();
+
+                    SetRedirectUri(BuildUri());
+                    core.Display.ShowMessage("Display Picture set", "You have successfully uploaded a new display picture.");
+                    return;
+                }
+                catch (GalleryItemTooLargeException)
+                {
+                    SetError("The photo you have attempted to upload is too big, you can upload photos up to 1.2 MiB in size.");
+                    return;
+                }
+                catch (GalleryQuotaExceededException)
+                {
+                    SetError("You do not have enough quota to upload this photo. Try resizing the image before uploading or deleting images you no-longer need. Smaller images use less quota.");
+                    return;
+                }
+                catch (InvalidGalleryItemTypeException)
+                {
+                    SetError("You have tried to upload a file type that is not a picture. You are allowed to upload PNG and JPEG images.");
+                    return;
+                }
+                catch (InvalidGalleryFileNameException)
+                {
+                    core.Display.ShowMessage("Submission failed", "Submission failed, try uploading with a different file name.");
+                    return;
+                }
+            }
+            else
+            {
+                DisplayGenericError();
+                return;
+            }
+        }
+    }
+}
